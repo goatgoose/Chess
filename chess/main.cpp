9cc74809eff9e6f3f36b01d985f56239c1e2cfe4
Sam Clark <sam.clark1142@gmail.com>
2016-12-10 11:56:34 -0500

    todo update

--- chess/main.cpp
+++ chess/main.cpp
@@ -20,22 +20,7 @@
 #include <Button.hpp>
 
 // TODO
-// call update to all clickables- dont manually update
-// button class
-//   make scalable
-//   positioned in percents
-// multiplayer server
-//   matchmaking / elo system ?
-// moves log (new window?)
-// host / join / find game menu
-//   go to this first before actual chess game
-// timer system
-// capture functionality
-// show captured pieces
-// test for checkmate every move, if no possible moves u lose
-// in game buttons
-//   surrender
-//   ask for draw (won't effect mmr)
+// https://docs.google.com/spreadsheets/d/1IAaIWOks1xy_ooIK4-HwEAsOXJJnhxrV7eh4zF4xzxc/edit?usp=sharing
 
 using namespace sf;
 using namespace std;
